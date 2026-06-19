0e5e31af82310f9db6f1992f9b5e81f2dd44051d
Flavio Mammoliti <flavio.mammoliti@gmail.com>
2026-06-19 19:50:50 +0200

    Project updated to 5.8

--- Source/UE_IoroomProject.Target.cs
+++ Source/UE_IoroomProject.Target.cs
@@ -8,8 +8,8 @@ public class UE_IoroomProjectTarget : TargetRules
 	public UE_IoroomProjectTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("UE_IoroomProject");
 	}
 }
--- Source/UE_IoroomProjectEditor.Target.cs
+++ Source/UE_IoroomProjectEditor.Target.cs
@@ -8,8 +8,8 @@ public class UE_IoroomProjectEditorTarget : TargetRules
 	public UE_IoroomProjectEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("UE_IoroomProject");
 	}
 }
